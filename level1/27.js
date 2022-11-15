8aa48d3bc19b588259f8725b852710eff2b866ef
Suji Chae <63655826+suji8073@users.noreply.github.com>
2022-11-15 23:59:44 +0900

    level 1 26~28

---
+++ level1/27.js
@@ -0,0 +1,19 @@
+/* 27. 3진법 뒤집기
+문제 설명
+자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+
+제한사항
+n은 1 이상 100,000,000 이하인 자연수입니다. */
+
+function solution(n) {
+  var array = [];
+  while (n >= 1) {
+    array.push(n % 3);
+    n = parseInt(n / 3);
+  }
+  var num = 0;
+  array.reverse().map((item, index) => {
+    num += item * 3 ** index;
+  });
+  return num;
+}
---
+++ level1/28.js
@@ -0,0 +1,19 @@
+/* 28. 두 개 뽑아서 더하기
+문제 설명
+정수 배열 numbers가 주어집니다. numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+
+제한사항
+numbers의 길이는 2 이상 100 이하입니다.
+numbers의 모든 수는 0 이상 100 이하입니다. */
+
+function solution(numbers) {
+  var array = [];
+  for (var i = 0; i < numbers.length; i++) {
+    for (var j = 1; j < numbers.length - 1; j++) {
+      array.push(numbers[i] * numbers[j]);
+    }
+  }
+  console.log([...new Set(array)]);
+}
+
+solution([2, 1, 3, 4, 1]);
